621ebb936f0a90ab0ad50d80a1af23dfdcd0dbcc
Timothy Ogbemudia <ogbemudiatimothy@gmail.com>
2021-11-22 14:38:03 +0100

    chore: sync branches

--- lib/registration.dart
+++ lib/registration.dart
@@ -2,9 +2,7 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:supabase/supabase.dart';
-import 'package:tru_sdk_flutter/tru_sdk_flutter.dart';
 import 'package:http/http.dart' as http;
-import 'package:supabase_flutter_phonecheck/models.dart';
 import 'package:supabase_flutter_phonecheck/helpers/supabase.dart';
 
 final String baseURL = 'https://witty-falcon-83.loca.lt';
@@ -16,70 +14,6 @@ class Registration extends StatefulWidget {
   _RegistrationState createState() => _RegistrationState();
 }
 
-Future<PhoneCheck?> createPhoneCheck(String phoneNumber) async {
-  final response = await http.post(Uri.parse('$baseURL/phone-check'),
-      body: {"phone_number": phoneNumber});
-
-  if (response.statusCode != 200) {
-    return null;
-  }
-  final String data = response.body;
-  return phoneCheckFromJSON(data);
-}
-
-Future<PhoneCheckResult?> getPhoneCheck(String checkId) async {
-  final response =
-      await http.get(Uri.parse('$baseURL/phone-check?check_id=$checkId'));
-
-  if (response.statusCode != 200) {
-    return null;
-  }
-
-  final String data = response.body;
-  return phoneCheckResultFromJSON(data);
-}
-
-Future<void> errorHandler(BuildContext context, String title, String content) {
-  return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(title),
-          content: Text(content),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'Cancel'),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        );
-      });
-}
-
-Future<void> successHandler(BuildContext context) {
-  return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('Registration Successful.'),
-          content: const Text('✅'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'Cancel'),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        );
-      });
-}
 
 class _RegistrationState extends State<Registration> {
 
